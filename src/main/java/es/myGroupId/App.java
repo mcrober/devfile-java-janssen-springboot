a15cd80825184fff6db2554b360855b93688caf8
X47019SE <roberto.segoviarodrigo@colaborador.elcorteingles.es>
2025-03-23 19:46:52 +0100

    cwi config map variables

--- src/main/java/es/myGroupId/App.java
+++ src/main/java/es/myGroupId/App.java
@@ -5,11 +5,22 @@ package es.myGroupId;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Map;
+
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
+
+        Map<String, String> map = System.getenv();
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            if (entry.getKey().contains("cwi")) {
+                System.out.println(entry.getKey() + "/" + entry.getValue());
+            }
+        }
+
         SpringApplication.run(App.class, args);
+
     }
 
 }
